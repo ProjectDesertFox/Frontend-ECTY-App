d6874600ef987b9451aa0cd101ae5ebdcac9999a
Muhammad Iqbal <muhammadiqbalsanusi@gmail.com>
2022-03-10 20:27:34 +0800

    add: setting, tabscreen icon

--- App.js
+++ App.js
@@ -1,29 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NativeBaseProvider} from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
-import  {Login} from './screens/Login';
-import { Register } from './screens/Register';
+// import  {Login} from './screens/Login';
+// import { Register } from './screens/Register';
 import { Home } from './screens/Home';
 import { Itinerary } from './screens/Itinerary';
 import { Friends } from './screens/Friends';
 import { Setting } from './screens/Setting';
 
+import { Icon } from 'react-native-elements';
+
 const Tab = createBottomTabNavigator()
 export default function App() {
   return (
-    
     <NativeBaseProvider>
-      <Login/>
+      {/* <Login/> */}
       <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Itinerary" component={Itinerary} />
-        <Tab.Screen name="Friends" component={Friends} />
-        <Tab.Screen name="Settings" component={Setting} />
-      </Tab.Navigator>
-    </NavigationContainer>
+        <Tab.Navigator tabBarOptions={{activeTintColor : '#00CEC9'}}>
+          <Tab.Screen name="Home" component={Home} options={{
+            headerShown: true,
+            tabBarIcon: (props) => (
+              <Icon type='feather' name='home' color={props.color}/>
+            ),
+          }} />
+          <Tab.Screen name="Itinerary" component={Itinerary} options={{
+            headerShown: true,
+            tabBarIcon: (props) => (
+              <Icon type='feather' name='check-circle' color={props.color} />
+            ),
+          }}/>
+          <Tab.Screen name="Friends" component={Friends} options={{
+            headerShown: true,
+            tabBarIcon: (props) => (
+              <Icon type='feather' name='user'color={props.color} />
+            ),
+          }} />
+          <Tab.Screen name="Settings" component={Setting} options={{
+            headerShown: true,
+            tabBarIcon: (props) => (
+              <Icon type='feather' name='settings' color={props.color} />
+            ),
+          }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   )
 }
--- screens/Setting.js
+++ screens/Setting.js
@@ -1,8 +1,43 @@
-import { Text, View } from "react-native";
+import { Divider, Image, Center, Box, Text } from "native-base";
 export const Setting = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
-        </View>
+        <Box mt={5}>
+            <Center>
+                <Image
+                    size={150}
+                    resizeMode="cover"
+                    source={{
+                        uri: "https://wallpaperaccess.com/full/317501.jpg"
+                    }}
+                    alt={"Alternate Text"}
+                    borderRadius={100} />
+                    <Text mt={5} fontWeight='bold' color='yellow.500' bold fontSize="md"> Premium </Text>
+            </Center>
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+                <Text fonstSize='lg'>Username</Text>
+                <Text fonstSize='lg'>IqbalSanusi</Text>
+            </Box>
+            <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+                <Text fonstSize='lg'>Email</Text>
+                <Text fonstSize='lg'>desert@fox.com</Text>
+            </Box>
+            <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+                <Text fonstSize='lg'>Phone</Text>
+                <Text fonstSize='lg'>0812345678910</Text>
+            </Box>
+            <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+                <Text fonstSize='lg'>Date of Birth</Text>
+                <Text fonstSize='lg'>12-12-2022</Text>
+            </Box>
+            <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+                <Text fonstSize='lg'>Address</Text>
+                <Text fonstSize='lg'>Canberra ACT2601, Australia</Text>
+            </Box>
+            <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
+        </Box>
     )
 }
