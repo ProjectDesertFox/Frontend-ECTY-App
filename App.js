8265b99984c334fb50e2e90532f6051f6c403e94
Muhammad Iqbal <muhammadiqbalsanusi@gmail.com>
2022-03-10 23:14:42 +0800

    add : temporary button's login

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, Stack} from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
-// import  {Login} from './screens/Login';
-// import { Register } from './screens/Register';
+import { Login } from './screens/Login';
+import { Register } from './screens/Register';
 import { Home } from './screens/Home';
 import { Itinerary } from './screens/Itinerary';
 import { Friends } from './screens/Friends';
-import { Setting } from './screens/Setting';
+import  Setting  from './screens/Setting';
 import store from './store';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux'
 import { Icon } from 'react-native-elements';
 
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
+
 export default function App() {
   return (
     <Provider store={store}>
       <StatusBar />
       <NativeBaseProvider>
-        {/* <Login/> */}
         <SafeAreaView style={{ flex: 1 }}>
           <NavigationContainer>
+            {/* <Stack.Navigator>
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Register" component={Register} />
+            </Stack.Navigator> */}
+
             <Tab.Navigator tabBarOptions={{ activeTintColor: '#00CEC9' }}>
               <Tab.Screen name="Home" component={Home} options={{
                 headerShown: true,
@@ -44,10 +52,19 @@ export default function App() {
               }} />
               <Tab.Screen name="Settings" component={Setting} options={{
                 headerShown: true,
+                title : 'Settings',
                 tabBarIcon: (props) => (
                   <Icon type='feather' name='settings' color={props.color} />
                 ),
               }} />
+              <Tab.Screen name="Register" component={Register} options={{
+                headerShown: true,
+                title : 'Register',
+                tabBarIcon: (props) => (
+                  <Icon type='feather' name='log-in' color={props.color} />
+                ),
+              }} />
+
             </Tab.Navigator>
           </NavigationContainer>
         </SafeAreaView>
--- screens/Setting.js
+++ screens/Setting.js
@@ -1,5 +1,5 @@
-import { Divider, Image, Center, Box, Text } from "native-base";
-export const Setting = () => {
+import { Divider, Image, Center, Box, Text, Button } from "native-base";
+export default function Profile (props) {
     return (
         <Box mt={5}>
             <Center>
@@ -11,33 +11,44 @@ export const Setting = () => {
                     }}
                     alt={"Alternate Text"}
                     borderRadius={100} />
-                    <Text mt={5} fontWeight='bold' color='yellow.500' bold fontSize="md"> Premium </Text>
+                <Text mt={5} fontWeight='bold' color='yellow.500' bold fontSize="md"> Premium </Text>
             </Center>
-            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}>
                 <Text fonstSize='lg'>Username</Text>
                 <Text fonstSize='lg'>IqbalSanusi</Text>
             </Box>
             <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
-            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}>
                 <Text fonstSize='lg'>Email</Text>
                 <Text fonstSize='lg'>desert@fox.com</Text>
             </Box>
             <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
-            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}>
                 <Text fonstSize='lg'>Phone</Text>
                 <Text fonstSize='lg'>0812345678910</Text>
             </Box>
             <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
-            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}>
                 <Text fonstSize='lg'>Date of Birth</Text>
                 <Text fonstSize='lg'>12-12-2022</Text>
             </Box>
             <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
-            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}> 
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}>
                 <Text fonstSize='lg'>Address</Text>
                 <Text fonstSize='lg'>Canberra ACT2601, Australia</Text>
             </Box>
             <Divider my="2" mx="5" bg="#DFE6E9" thickness="2" />
+            <Box Flex flexDirection='row' justifyContent='space-between' px={5} mt={5}>
+                {/* <Button colorScheme="success"
+                onPress={() => navigation.navigate("Login", {
+                })}
+                >
+                    Login
+                </Button> */}
+                <Button colorScheme="success">
+                    Register
+                </Button>
+            </Box>
         </Box>
     )
 }
