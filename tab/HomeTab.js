b6b756b91ee2e30ba5e8b033acd4d05128708457
bellawatsonf <bellawatsonf@gmail.com>
2022-03-14 20:20:39 +0700

    fix hometab

--- tab/HomeTab.js
+++ tab/HomeTab.js
@@ -3,8 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DetailCity } from "../screens/DetailCity";
 import { DetailDestination } from '../screens/DetailDestination';
 import { Home } from "../screens/Home";
-import { Login } from '../screens/Login';
-import { Register } from '../screens/Register';
+
 
 export const HomeTab=()=>{
     const Stack = createNativeStackNavigator();
@@ -14,8 +13,7 @@ export const HomeTab=()=>{
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="DetailCity" component={DetailCity} />
                 <Stack.Screen name="DetailDestination" component={DetailDestination} />
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Register" component={Register} />
+                
                 {/* <Stack.Screen name="Transportation" component={Transportation} />
                 <Stack.Screen name="ItineraryVerifikasi" component={ItineraryVerifikasi} /> */}
 
